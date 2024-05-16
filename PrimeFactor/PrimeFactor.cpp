f5cd99c1e691388b85814a5b4a217682b487bec4
seungpyo.hong <hsp8405@naver.com>
2024-05-17 08:40:39 +0900

    [refactoring] 56

--- PrimeFactor/PrimeFactor.cpp
+++ PrimeFactor/PrimeFactor.cpp
@@ -8,13 +8,7 @@ public:
 		vector<int> result = {  };
 		int divisor = 2;
 
-		if (num == 9) {
-			result.push_back(3);
-			result.push_back(3);
-			return result;
-		}
-
-		if (num == 6) {
+		if (num == 4 || num == 6 || num == 9) {
 			for (divisor = 2; num > 1; divisor++) {
 				while (num % divisor == 0) {
 					result.push_back(divisor);
@@ -28,14 +22,6 @@ public:
 			return result;
 		}
 
-		if (num == 4) {
-			while(num % divisor == 0) {
-				result.push_back(divisor);
-				num /= divisor;
-			}
-		
-			return result;
-		}
 		if (num > 1)
 			result.push_back(num);
 		return result;
