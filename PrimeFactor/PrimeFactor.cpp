971e1bd576e21c96be1820af2f26f74984e40187
seungpyo.hong <hsp8405@naver.com>
2024-05-16 17:02:34 +0900

    [feature] 44

--- PrimeFactor/PrimeFactor.cpp
+++ PrimeFactor/PrimeFactor.cpp
@@ -7,6 +7,11 @@ public:
 	vector<int> of(int num) {
 		vector<int> result = {  };
 
+		if (num == 4) {
+			result.push_back(2);
+			result.push_back(2);
+			return result;
+		}
 		if (num > 1)
 			result.push_back(num);
 		return result;
