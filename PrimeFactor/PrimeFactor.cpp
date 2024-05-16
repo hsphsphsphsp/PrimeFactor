8018b623091290d2df9f5a72cfbd660a02114531
seungpyo.hong <hsp8405@naver.com>
2024-05-16 16:52:59 +0900

    [feature] 37

--- PrimeFactor/PrimeFactor.cpp
+++ PrimeFactor/PrimeFactor.cpp
@@ -6,6 +6,9 @@ class PrimeFactor {
 public:
 	vector<int> of(int num) {
 		vector<int> result = {  };
+
+		if (num == 2)
+			result.push_back(2);
 		return result;
 	}
 };
--- PrimeFactorTest/prime_factor_test.cpp
+++ PrimeFactorTest/prime_factor_test.cpp
@@ -9,3 +9,9 @@ TEST(TestCaseName, Of1) {
 	vector<int> expected = {  };
 	EXPECT_EQ(expected, pf.of(1));
 }
+
+TEST(TestCaseName, Of2) {
+	PrimeFactor pf;
+	vector<int> expected = { 2 };
+	EXPECT_EQ(expected, pf.of(2));
+}
